bd585b4c5cb21202b6dd7ca65076aed833f45c14
sohyeon <t01081418141@naver.com>
2022-03-14 15:12:36 +0900

    🎨 Change footer size

--- frontend/src/layouts/footer/Footer.js
+++ frontend/src/layouts/footer/Footer.js
@@ -1,8 +1,16 @@
+import { styled } from '@mui/material/styles';
+import { Box} from '@mui/material';
 
 const Footer = () => {
+  const Wrapper = styled(Box)(() => ({
+    width:'100%',
+    height:'100px',
+    backgroundColor:'#111111',
+  }));
 
   return (
-    <h1>Footer</h1>
+    <Wrapper>
+    </Wrapper>
   );
 };
 
