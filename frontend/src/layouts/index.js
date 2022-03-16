383d9fc5287337105beb585eba8a5dcc7a58b9bb
sohyeon <t01081418141@naver.com>
2022-03-16 14:07:40 +0900

    🎨 Change layout padding

--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -18,13 +18,13 @@ const MainLayout = () => {
     flexGrow: 1,
     overflow: 'auto',
     minHeight: '100%',
-    paddingTop: APP_BAR_MOBILE + 24,
-    paddingBottom: theme.spacing(10),
-    [theme.breakpoints.up('lg')]: {
-      paddingTop: APP_BAR_DESKTOP + 24,
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2)
-    }
+    // paddingTop: APP_BAR_MOBILE + 24,
+    // paddingBottom: theme.spacing(10),
+    // [theme.breakpoints.up('lg')]: {
+    //   paddingTop: APP_BAR_DESKTOP + 24,
+    //   paddingLeft: theme.spacing(2),
+    //   paddingRight: theme.spacing(2)
+    // }
   }));
 
   return (
