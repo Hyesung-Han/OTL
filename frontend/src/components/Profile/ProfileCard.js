0fa938e3c25a0fb293864d7ac5d12cf69885178d
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-03-17 16:29:21 +0900

    🔨Update ProfileCard

--- frontend/src/components/Profile/ProfileCard.js
+++ frontend/src/components/Profile/ProfileCard.js
@@ -4,40 +4,44 @@ import { Box, Card, Link, Typography, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Avatar from '@mui/material/Avatar';
 
+const BoxStyle = styled('Box')({
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+    position: 'absolute'
+  });
 
+  
 const CardStyle = styled(Card)({
-  borderRadius : 8,
   "&:hover":{
     boxShadow: '2px 5px 5px 2px rgba(225, 223, 214, 1)'
   }
 });
 
 
-ItemsCard.propTypes = {
+ProfileCard.propTypes = {
   product: PropTypes.object
 };
 
 /**
- * CSW | 2022.03.17 | ADD
+ * CSW | 2022.03.17 | FIX
  * @name ProfileCard
  * @des ProfileCard 컴포넌트
  */
 export default function ProfileCard({ product }) {
-  // 이미지, 제목, 가격, 토큰 ID, 심볼
   const { image, nickname } = product;
-  const symbol = 'ETH';
 
   return (
     <CardStyle>
       <Link
-          to={`/#/${tokenId}`}
+          to={`/#`}
           color="inherit"
           underline='hover'
           component={RouterLink}
         >
-        <Box sx={{ pt: '100%', position: 'relative'}}>
-          <Avatar alt={nickname} src={image} sx={{ width: 100, height: 100 }}/>
-        </Box>
+        <BoxStyle sx={{ pt: '100%', position: 'relative'}}>
+          <Avatar alt={nickname} src={image} sx={{ width: 150, height: 150, margin: 'auto', mt: 5 }}/>
+        </BoxStyle>
         <Stack spacing={2} sx={{ p: 3 }}>
             <Typography variant="subtitle1" textAlign="center" noWrap>
               {nickname}
