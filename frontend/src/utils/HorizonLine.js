43f59475574101b1f8085eba753499236905c269
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-03-17 16:28:37 +0900

    🔨Update HorizonLine

--- frontend/src/utils/HorizonLine.js
+++ frontend/src/utils/HorizonLine.js
@@ -1,18 +1,26 @@
 import React from "react";
 
+/**
+ * CSW | 2022.03.17 | FIX
+ * @name HorizonLine
+ * @des HorizonLine 수평선 필요한 곳에 추가
+ */
 const HorizonLine = ({ text }) => {
   return (
-    <div
-      style={{
-        width: "100%",
-        textAlign: "center",
-        borderBottom: "1px solid #aaa",
-        lineHeight: "0.1em",
-        margin: "10px 0 20px",
-      }}
-    >
-      <span style={{ background: "#fff", padding: "0 10px" }}>{text}</span>
+    <div>
+        <span style={{ background: "#fff", padding: "0 10px",fontWeight:'bold' }}>{text}</span>
+        <div
+        style={{
+            width: "100%",
+            textAlign: "center",
+            borderBottom: "1px solid #bcbcbc",
+            lineHeight: "0.1em",
+            margin: "10px 0 20px",
+        }}
+         >
+        </div>
     </div>
+    
   );
 };
 
