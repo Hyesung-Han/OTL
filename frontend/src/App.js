c0606591b962d80c03d84cc73ebd732322cfe23c
LDJ <ehdwnsdl1210@gmail.com>
2022-03-21 16:02:30 +0900

    🔨 Update App.js add serverUrlBase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import Router from './routes';
-import ThemeConfig from './theme';
-import GlobalStyles from './theme/globalStyles';
+import Router from "./routes";
+import ThemeConfig from "./theme";
+import GlobalStyles from "./theme/globalStyles";
+
+// hook
+import { CommonContext } from "./context/CommonContext";
+
+const HOST = "localhost:3000/api";
+const serverUrl = `http://${HOST}/v1`; // 사용 X, 큰 의미 없음
+const serverUrlBase = `http://${HOST}`;
 
 export default function App() {
   return (
-    <ThemeConfig>
-      <GlobalStyles />
-      <Router />
-    </ThemeConfig>
+    <CommonContext.Provider
+      value={{
+        serverUrl,
+        serverUrlBase,
+      }}
+    >
+      <ThemeConfig>
+        <GlobalStyles />
+        <Router />
+      </ThemeConfig>
+    </CommonContext.Provider>
   );
 }
