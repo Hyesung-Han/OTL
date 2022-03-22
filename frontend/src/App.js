931173e842d24110696bd66e4e9909719124741b
LDJ <ehdwnsdl1210@gmail.com>
2022-03-22 10:54:12 +0900

    🔨 Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,12 @@ import GlobalStyles from "./theme/globalStyles";
 import { CommonContext } from "./context/CommonContext";
 
 const HOST = "localhost:3000/api";
-const serverUrl = `http://${HOST}/v1`; // 사용 X, 큰 의미 없음
 const serverUrlBase = `http://${HOST}`;
 
 export default function App() {
   return (
     <CommonContext.Provider
       value={{
-        serverUrl,
         serverUrlBase,
       }}
     >
