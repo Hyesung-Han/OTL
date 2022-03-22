b107d74260ae8c80c69b8a8a8f5426a555e75a90
sohyeon <t01081418141@naver.com>
2022-03-22 16:01:00 +0900

    🔧Add buynow, makeoffer, duration to registerSale

--- frontend/src/pages/RegisterSale.js
+++ frontend/src/pages/RegisterSale.js
@@ -1,11 +1,24 @@
 import { Link as RouterLink } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { Grid, Box, Button, Typography, Divider,Paper,InputBase } from "@mui/material";
+import {
+  Grid,
+  Box,
+  Button,
+  Typography,
+  Divider,
+  Paper,
+  InputBase,
+} from "@mui/material";
 import Page from "../components/Page";
 import { useState } from "react";
 
 import logo from "../image/logo.png";
 
+/**
+ * HSH | 2022.03.22 | ADD
+ * @name RegisterSale
+ * @des Register Sale 페이지. 판매 등록
+ */
 function RegisterSale() {
   const { item_id } = useParams();
   // console.log(item_id);
@@ -14,34 +27,29 @@ function RegisterSale() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  /**
-   * HSH | 2022.03.22 | v1.0
-   * @name ItemDetailGridStyle
-   * @des 이미지, author, title, description이 들어가는 영역
-   */
+  const [buyNow, setBuyNow] = useState("");
+  const [makeOffer, setMakeOffer] = useState("");
+  const [endDate, setendDate] = useState("");
+
+  const itemSize = "320px";
+
   const ItemDetailGridStyle = {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-around",
-    alignItems:"center",
+    alignItems: "center",
   };
 
-  /**
-   * HSH | 2022.03.22 | v1.0
-   * @name ItemSaleGridStyle
-   * @des buy now, make offer, end date가 들어가는 영역
-   */
-  const ItemSaleGridStyle = {};
+  const ItemSaleGridStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  };
 
-  /**
-   * HSH | 2022.03.22 | v1.0
-   * @name DetailImgGridStyle
-   * @des ItemDetailGridStyle안에서 Image가 들어가는 영역 css
-   */
   const DetailImgGridStyle = {
-    margin: 3,
-    width: "350px",
-    height: "350px",
+    width: itemSize,
+    height: itemSize,
 
     display: "flex",
     justifyContent: "center",
@@ -51,16 +59,11 @@ function RegisterSale() {
     borderRadius: "20px",
   };
 
-  /**
-   * HSH | 2022.03.22 | v1.0
-   * @name DetailGridStyle
-   * @des ItemDetailGridStyle안에서 Author, title, description이 들어가는 영역
-   */
   const DetailGridStyle = {
-    margin: 5,
+    margin: 3,
   };
 
-  const TextFiledList = [
+  const itemDetailList = [
     {
       name: "Author",
       value: author,
@@ -77,6 +80,54 @@ function RegisterSale() {
       rows: 4,
     },
   ];
+
+  const saleData = [
+    {
+      name: "Buy Now",
+      unit: "ETH",
+      value: buyNow,
+      func: onChangeBuyNow,
+    },
+    {
+      name: "Make Offer",
+      unit: "ETH",
+      value: makeOffer,
+      func: onChangeMakeOffer,
+    },
+    {
+      name: "Duration",
+      unit: "DAY",
+      value: endDate,
+      func: onChangeEndDate,
+    },
+  ];
+
+  /**
+   * HSH | 2022.03.22 | v1.0
+   * @name onChangeBuyNow
+   * @des buy now 값 변경 시 실행
+   */
+  function onChangeBuyNow(e) {
+    setBuyNow(e.target.value);
+  }
+
+  /**
+   * HSH | 2022.03.22 | v1.0
+   * @name onChangeMakeOffer
+   * @des make offer 값 변경 시 실행
+   */
+  function onChangeMakeOffer(e) {
+    setMakeOffer(e.target.value);
+  }
+
+  /**
+   * HSH | 2022.03.22 | v1.0
+   * @name onChangeEndDate
+   * @des end date 값 변경 시 실행
+   */
+  function onChangeEndDate(e) {
+    setendDate(e.target.value);
+  }
 
   return (
     <Page display="flex" justifyContent="center" title="SSAFY NFT">
@@ -96,38 +147,62 @@ function RegisterSale() {
           <Grid sx={DetailImgGridStyle}>
             <Box
               component="img"
-              sx={{ maxWidth: "350px", maxHeight: "350px" }}
+              sx={{ maxWidth: itemSize, maxHeight: itemSize }}
               src={logo}
             />
           </Grid>
           <Grid sx={DetailGridStyle}>
-              {TextFiledList.map((item,index)=>(
-                 <Box key={index} mb={3}>
-                 <Typography variant="h5">{item.name}</Typography>
-                 <Paper
-                   sx={{
-                     display: "flex",
-                     justifyContent: "center",
-           
-                     p: "2px 4px",
-                     width: 500,
-                     border: "2px solid #cbcbcb",
-                     backgroundColor:"#dfdfdf",
-                   }}
-                 >
-                   <InputBase
-                     value={item.value}
-                     multiline={(item.rows===1)?false:true}
-                     rows={item.rows}
-                     sx={{ width: "95%" }}
-                     disabled
-                   />
-                 </Paper>
-               </Box> 
-              ))}
+            {itemDetailList.map((item, index) => (
+              <Box key={index} mb={3}>
+                <Typography variant="h5">{item.name}</Typography>
+                <Paper
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+
+                    p: "2px 4px",
+                    width: 500,
+                    border: "2px solid #cbcbcb",
+                    backgroundColor: "#dfdfdf",
+                  }}
+                >
+                  <InputBase
+                    value={item.value}
+                    multiline={item.rows === 1 ? false : true}
+                    rows={item.rows}
+                    sx={{ width: "95%" }}
+                    readOnly
+                  />
+                </Paper>
+              </Box>
+            ))}
           </Grid>
         </Grid>
-        <Grid sx={ItemSaleGridStyle}></Grid>
+        <Grid sx={ItemSaleGridStyle}>
+          {saleData.map((item, index) => (
+            <Box key={index} mb={3}>
+              <Typography variant="h5">{item.name}</Typography>
+              <Paper
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+
+                  p: "2px 5px",
+                  border: "2px solid #cbcbcb",
+                  width: 900,
+                }}
+              >
+                <InputBase
+                  onChange={item.func}
+                  value={item.value}
+                  sx={{ width: "95%" }}
+                />
+                <Typography>{item.unit}</Typography>
+              </Paper>
+            </Box>
+          ))}
+        </Grid>
       </Grid>
     </Page>
   );
