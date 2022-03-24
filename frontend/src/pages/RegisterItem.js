a74b261a93999c54f7005d156f12ed5f8b592087
sohyeon <t01081418141@naver.com>
2022-03-24 12:00:48 +0900

    🔨Update registerItem page valid

--- frontend/src/pages/RegisterItem.js
+++ frontend/src/pages/RegisterItem.js
@@ -16,7 +16,7 @@ import {
 } from "@mui/material";
 import Page from "../components/Page";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import BackupIcon from "@mui/icons-material/Backup";
 import logo from "../image/logo.png";
@@ -35,49 +35,15 @@ function RegisterItem() {
   const [titleError, setTitleError] = useState(true);
   const [descriptionError, setDescriptionError] = useState(true);
 
-  const RootStyle = {
-    width: "100%",
+  const [isValid, setIsValid] = useState(false);
 
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  };
-
-  const BodyStyle = {
-    width: 1000,
-  };
-
-  const ContentStyle = {
-    display: "flex",
-    alignItems: "left",
-    flexDirection: "column",
-
-    padding: "0 50px",
-  };
-
-  const ImageStyle = styled(Button)((props) => ({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-
-    padding: "10px",
-
-    width: "400px",
-    height: "400px",
-
-    border: "dashed #ababab",
-    borderRadius: "20px",
-  }));
-
-  /**
-   * HSH | 2022.03.21 | v1.0
-   * @name onClickImg
-   * @des 이미지 클릭 시 실행되는 함수
-   */
-  const onClickImg = () => {
-    imgRef.current.click();
-  };
+  useEffect(() => {
+    if (!imgError && !authorError && !titleError && !descriptionError) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [imgError, authorError, titleError, descriptionError]);
 
   /**
    * HSH | 2022.03.21 | v1.0
@@ -104,10 +70,9 @@ function RegisterItem() {
     const value = e.target.value;
     setTitle(e.target.value);
 
-    if(!value){
+    if (!value) {
       setTitleError(true);
-    }
-    else{
+    } else {
       setTitleError(false);
     }
   };
@@ -121,10 +86,9 @@ function RegisterItem() {
     const value = e.target.value;
     setDescription(e.target.value);
 
-    if(!value){
+    if (!value) {
       setDescriptionError(true);
-    }
-    else{
+    } else {
       setDescriptionError(false);
     }
   };
@@ -166,9 +130,111 @@ function RegisterItem() {
 
   /**
    * HSH | 2022.03.21 | v1.0
-   * @name inputTextList
-   * @des inputTexts의 값들을 뿌려주는 컴포넌트
+   * @name categoryList
+   * @des 카테고리 리스트
+   * HACK
+   * 카테고리 db에서 받아오기전까지는 임시로 사용하도록 만들어 놓음
    */
+  const categoryList = [
+    {
+      id: 0,
+      name: "chair",
+    },
+    {
+      id: 1,
+      name: "table",
+    },
+    {
+      id: 2,
+      name: "wallpaper",
+    },
+    {
+      id: 3,
+      name: "floor",
+    },
+    {
+      id: 4,
+      name: "wall hanging",
+    },
+    {
+      id: 5,
+      name: "prop",
+    },
+  ];
+
+  /**
+   * HSH | 2022.03.21 | v1.0
+   * @name onClickImg
+   * @des 이미지 클릭 시 실행되는 함수
+   */
+  const onClickImg = () => {
+    imgRef.current.click();
+  };
+  /**
+   * HSH | 2022.03.21 | v1.0
+   * @name onClickCreate
+   * @des create 버튼 클릭 시 실행
+   */
+  const onClickCreate = () => {
+    console.log("onClickCreate");
+  };
+
+  /**
+   * HSH | 2022.03.21 | v1.0
+   * @name onClickCancel
+   * @des cancel 버튼 클릭 시 실행
+   */
+  const onClickCancel = () => {
+    console.log("onClickCancel");
+    navigate("/main");
+  };
+
+  /**
+   * HSH | 2022.03.21 | v1.0
+   * @name onImgChange
+   * @des 파일 선택장에서 파일 선택 시 실행
+   */
+  const onImgChange = async (event) => {
+    setUploadImg(URL.createObjectURL(event.target.files[0]));
+
+    setImgError(false);
+  };
+
+  const RootStyle = {
+    width: "100%",
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const BodyStyle = {
+    width: 1000,
+  };
+
+  const ContentStyle = {
+    display: "flex",
+    alignItems: "left",
+    flexDirection: "column",
+
+    padding: "0 50px",
+  };
+
+  const ImageStyle = styled(Button)((props) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+
+    padding: "10px",
+
+    width: "400px",
+    height: "400px",
+
+    border: "dashed #ababab",
+    borderRadius: "20px",
+  }));
+
   const inputTextList = inputTexts.map((item, index) => (
     <FormControl
       key={index}
@@ -203,8 +269,8 @@ function RegisterItem() {
         />
       </Paper>
       <Box ml={1}>
-      {item.error && <FormHelperText>This is a required item</FormHelperText>}
-      {!item.error && <FormHelperText>Valid</FormHelperText>}
+        {item.error && <FormHelperText>This is a required item</FormHelperText>}
+        {!item.error && <FormHelperText>Valid</FormHelperText>}
       </Box>
     </FormControl>
   ));
@@ -227,77 +293,6 @@ function RegisterItem() {
       color: "#00AB55",
     },
   }));
-
-  /**
-   * HSH | 2022.03.21 | v1.0
-   * @name onClickCreate
-   * @des create 버튼 클릭 시 실행
-   */
-  const onClickCreate = () => {
-    let isClear = true;
-    if (!uploadImg) {
-      isClear = false;
-    }
-
-    if (isClear) {
-      console.log("onClickCreate");
-    }
-  };
-
-  /**
-   * HSH | 2022.03.21 | v1.0
-   * @name onClickCancel
-   * @des cancel 버튼 클릭 시 실행
-   */
-  const onClickCancel = () => {
-    console.log("onClickCancel");
-    navigate("/main");
-  };
-
-  /**
-   * HSH | 2022.03.21 | v1.0
-   * @name onImgChange
-   * @des 파일 선택장에서 파일 선택 시 실행
-   */
-  const onImgChange = async (event) => {
-    setUploadImg(URL.createObjectURL(event.target.files[0]));
-
-    setImgError(false);
-  };
-
-  /**
-   * HSH | 2022.03.21 | v1.0
-   * @name categoryList
-   * @des 카테고리 리스트
-   * HACK
-   * 카테고리 db에서 받아오기전까지는 임시로 사용하도록 만들어 놓음
-   */
-  const categoryList = [
-    {
-      id: 0,
-      name: "chair",
-    },
-    {
-      id: 1,
-      name: "table",
-    },
-    {
-      id: 2,
-      name: "wallpaper",
-    },
-    {
-      id: 3,
-      name: "floor",
-    },
-    {
-      id: 4,
-      name: "wall hanging",
-    },
-    {
-      id: 5,
-      name: "prop",
-    },
-  ];
 
   return (
     <div style={RootStyle}>
@@ -330,7 +325,9 @@ function RegisterItem() {
                 </div>
               )}
             </ImageStyle>
-            {!uploadImg && (<Typography color="#ff0000"> * required Image </Typography>) }
+            {!uploadImg && (
+              <Typography color="#ff0000"> * required Image </Typography>
+            )}
             <input
               ref={imgRef}
               type="file"
@@ -363,7 +360,9 @@ function RegisterItem() {
               </FormControl>
             </Box>
             <Box mt={3} display="flex" justifyContent="right">
-              <ButtonStyle onClick={onClickCreate}>CREATE</ButtonStyle>
+              <ButtonStyle disabled={!isValid} onClick={onClickCreate}>
+                CREATE
+              </ButtonStyle>
               <ButtonStyle onClick={onClickCancel}>CANCEL</ButtonStyle>
             </Box>
           </div>
