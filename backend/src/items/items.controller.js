491562699d8d58eeec3103f2b4fba86e20b69b02
한혜성 <hanhs4544@naver.com>
2022-03-24 14:49:48 +0900

    🔧Add home MVC file

--- backend/src/items/items.controller.js
+++ backend/src/items/items.controller.js
@@ -168,22 +168,4 @@ router.get('/:token_id', async function (req, res) {
 	}
 });
 
-/**
- * HHS | 2022.03.24 | v1.0
- * @name items
- * @api {get} /items?owner_address=address
- * @des
- * 사용자의 마이룸 꾸미기에 사용된 아이템 목록 가져오기
- */
-router.get('/:owner_address', async function (req, res) {
-	try {
-		const { statusCode, responseBody } = await itemService.getHomeByOwnerAddress(req.query['owner_address']);
-
-		res.statusCode = statusCode;
-		res.send(responseBody);
-	} catch(e) {
-		console.error("getHomeByOwnerAddress",e);
-		res.status(403).send({result:"fail", error:e});
-	}
-});
 module.exports = router;
