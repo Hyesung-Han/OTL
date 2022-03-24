445485d7074d9382085ad5fc3d613b6a334ea8d5
LDJ <ehdwnsdl1210@gmail.com>
2022-03-24 10:33:36 +0900

    🔧 Add CreateProfile api & reg function

---
+++ frontend/src/pages/CreateProfile.js
@@ -0,0 +1,272 @@
+import { useState, useRef, useEffect, useContext } from "react";
+import Page from "../components/Page";
+import { Link as RouterLink } from "react-router-dom";
+import { Avatar, Button, Grid, TextField, Typography } from "@mui/material";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import HorizonLine from "../components/HorizonLine";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+
+import { CommonContext } from "../context/CommonContext";
+
+import Axios from "axios";
+import Swal from "sweetalert2";
+
+// 닉네임 유효성 검사 (영소문자+숫자, 4자이상)
+const regNm = /^[a-z0-9]{4,}$/;
+
+// 이메일 유효성 검사 (대소문자 구분 X, 문자/숫자연속가능)
+const regEma =
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+/**
+ * LDJ | 2022.03.24 | ADD
+ * @name CreateProfile
+ * @api {post} 진행중
+ * @api {get} http://localhost:3000/api/user/checkNick?user_nickname={닉네임} : 닉네임 중복 확인
+ * @des 로그인 된 유저가 자신의 프로필을 생성하는 곳! (유효성 검사 + 중복 검사 + 내용 입력/저장)
+ */
+
+function CreateProfile() {
+  const user = useSelector((state) => state.User.user);
+  const dispatch = useDispatch();
+
+  const { serverUrlBase } = useContext(CommonContext);
+
+  const [disabled, setDisabled] = useState(true);
+
+  const [profileData, setProfileData] = useState({
+    nickname: "",
+    email: "",
+    link: "",
+    bio: "",
+  });
+
+  const OnChangeHandler = (name) => (e) => {
+    setProfileData({ ...profileData, [name]: e.target.value });
+  };
+
+  const [nincknameErr, setNicknameErr] = useState(false);
+  const [nincknameErrMsg, setNicknameErrMsg] = useState();
+  const [emailErr, setEmailErr] = useState(false);
+  const [emailErrMsg, setEmailErrMsg] = useState();
+
+  const onCreateHandler = async () => {
+    var { nickname, email, link, bio } = profileData;
+    /**
+     * TODO
+     * post api  이해해서 작성 더 해야함 (프로필 생성)
+     */
+    Axios.post(serverUrlBase + `/user/profile`, {
+      user_address: user.user_address,
+      user_nickname: nickname,
+      user_email: email,
+      user_link: link,
+      user_bio: bio,
+    })
+      .then((data) => {
+        console.log(data);
+        // const create_result = data.data.result;
+        // if (create_result == "success") {
+        //   console.log(data.data);
+        // } else {
+        //   console.log("실패");
+        // }
+      })
+      .catch(function (error) {
+        console.log("프로필 생성 오류 : " + error);
+      });
+  };
+
+  useEffect(() => {
+    if (profileData.nickname.length === 0) {
+      setNicknameErr(false);
+      setNicknameErrMsg();
+    } else {
+      if (!regNm.test(profileData.nickname)) {
+        setNicknameErr(true);
+        setNicknameErrMsg(
+          "Please enter lowercase letters + numbers and at least 4 characters!"
+        );
+      } else {
+        Axios.get(
+          serverUrlBase +
+            `/user/checkNick/?user_nickname=` +
+            profileData.nickname
+        )
+          .then((data) => {
+            // console.log(data);
+            if (data.data.result === false) {
+              setNicknameErr(true);
+              setNicknameErrMsg("It's a registered ID that already exists!");
+            } else {
+              setNicknameErr(false);
+              setNicknameErrMsg();
+            }
+          })
+          .catch(function (error) {
+            console.log("닉네임 중복 오류 : " + error);
+          });
+      }
+    }
+
+    if (profileData.email.length === 0) {
+      setEmailErr(false);
+      setEmailErrMsg();
+    } else {
+      if (!regEma.test(profileData.email)) {
+        setEmailErr(true);
+        setEmailErrMsg("Please enter it in the form of an e-mail!");
+      } else {
+        setEmailErr(false);
+        setEmailErrMsg();
+      }
+    }
+
+    if (
+      profileData.nickname !== "" &&
+      profileData.email !== "" &&
+      nincknameErr === false &&
+      emailErr === false
+    ) {
+      setDisabled(false);
+    }
+
+    if (
+      profileData.nickname === "" ||
+      profileData.email === "" ||
+      nincknameErr === true ||
+      emailErr === true
+    ) {
+      setDisabled(true);
+    }
+    console.log(profileData);
+  }, [profileData.nickname, profileData.email, nincknameErr, emailErr]);
+
+  return (
+    <Page
+      title="OTL"
+      maxWidth="100%"
+      minHeight="60vh"
+      alignItems="center"
+      justifyContent="center"
+      display="flex"
+    >
+      <Grid
+        sx={{ width: 1 / 3 }}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Typography variant="h2">Create a profile!</Typography>
+        <HorizonLine></HorizonLine>
+        <Typography variant="h3" sx={{ py: 2 }}>
+          Profile Image
+        </Typography>
+        <Typography variant="h5" sx={{ pb: 2 }}>
+          Recommended 350px * 350px / Max Size : 100MB
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justifycontent="flex-start"
+          alignItems="flex-end"
+        >
+          <Grid>
+            <Avatar sx={{ width: 200, height: 200, m: 3 }}></Avatar>
+          </Grid>
+          <Grid>
+            <Button variant="contained" sx={{ m: 3 }}>
+              Delete Img
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid item xs={12} sx={{ py: 3 }}>
+            <TextField
+              required
+              error={nincknameErr}
+              helperText={nincknameErrMsg}
+              defaultValue={profileData.nickname}
+              fullWidth
+              label="Enter nickname"
+              onChange={OnChangeHandler("nickname")}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ py: 3 }}>
+            <TextField
+              required
+              error={emailErr}
+              helperText={emailErrMsg}
+              defaultValue={profileData.email}
+              fullWidth
+              label="Enter email"
+              onChange={OnChangeHandler("email")}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ py: 3 }}>
+            <TextField
+              variant="filled"
+              disabled
+              fullWidth
+              label="MetaMask Address"
+              size="small"
+              value={user.user_address}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ py: 1 }}>
+            <TextField
+              fullWidth
+              label="Enter your Twitter"
+              onChange={OnChangeHandler("link")}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ py: 1 }}>
+            <TextField
+              fullWidth
+              label="Enter your Instargram"
+              // onChange={OnChangeHandler()}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ py: 1 }}>
+            <TextField
+              fullWidth
+              label="Enter yoursite.io"
+              // onChange={OnChangeHandler()}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ py: 4 }}>
+            <TextField
+              multiline
+              rows={4}
+              fullWidth
+              label="Tell the world your story!"
+              onChange={OnChangeHandler("bio")}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              disabled={disabled}
+              variant="contained"
+              sx={{ py: 1, mx: 2, mb: 3 }}
+              onClick={onCreateHandler}
+            >
+              Create
+            </Button>
+            <Button
+              to="/main"
+              component={RouterLink}
+              variant="contained"
+              sx={{ py: 1, mx: 2, mb: 3 }}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Page>
+  );
+}
+
+export default CreateProfile;
