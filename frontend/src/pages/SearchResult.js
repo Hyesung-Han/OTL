70e874517c75aae128cf305b093b33a577a3c40d
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-03-29 14:32:20 +0900

    🔧Add Search API

--- frontend/src/pages/SearchResult.js
+++ frontend/src/pages/SearchResult.js
@@ -1,8 +1,8 @@
 import { Box, Container, Typography } from '@mui/material';
 import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from "react";
 import { MotionContainer, varBounceIn } from '../components/animate';
-import axios from 'axios';
+import Axios from 'axios';
 import Web3 from 'web3';
 import COMMON_ABI from '../common/ABI';
 import COMMON_HEADER from '../common/HeaderType';
@@ -12,17 +12,23 @@ import Page from '../components/Page';
 import ItemsList from '../components/items/ItemsList';
 import ProfileList from '../components/profile/ProfileList';
 import HorizonLine from '../components/HorizonLine'
+import {CommonContext} from "../context/CommonContext"
+import { useParams } from 'react-router-dom';
 
 /**
- * [검색결과] 화면
+ * CSW | 2022.03.29 | UPDATE
+ * @name SearchResult
+ * @des SearchResult P
+ * @api {get} /search/:search_value
  */
 const SearchResult = () => {
   // [변수] 아이템, 컬렉션 유무, 로딩
+  const { serverUrlBase } = useContext(CommonContext);
   const [item, setItem] = useState([]);
   const [isCollection, setIsCollection] = useState(false);
   const [loading, setLoading] = useState(false);
   const [profile, setProfile] = useState([]);
-
+  const {search_value} = useParams();
 
   // Web3
   const web3 = new Web3(new Web3.providers.HttpProvider(process.env.REACT_APP_ETHEREUM_RPC_URL));
@@ -53,16 +59,21 @@ const SearchResult = () => {
      */
     setLoading(true);
     
-    const resultList = [];
-    const resultProfile = {
-      id: 1,
-      image: "",
-      nickname: "fake nickname"
-    };
+    try {
+      const resultList = [];
+      const res = await Axios.get(serverUrlBase + `/user/list/`,{
+        params:{user_nickname:search_value}
+      });
+      const data = res.data.data;
 
-    resultList.push(resultProfile);
 
-    setProfile(resultList);
+      console.log(data);
+      
+  } catch (e) {
+      console.log('getProfile error' +  e);
+  }
+
+    // setProfile(resultList);
     setLoading(false);
     setIsCollection(true);
   };
