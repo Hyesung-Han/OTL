9dd3eb7a106d61e5aa02bddf286c31c799a81a9e
sowonwow2@gmail.com <sowonwow2@gmail.com>
2022-03-30 16:23:56 +0900

    🔧Add NFT ABI

--- frontend/src/components/items/ItemsList.js
+++ frontend/src/components/items/ItemsList.js
@@ -4,6 +4,7 @@ import ItemsCard from './ItemsCard';
 
 ItemsCard.propTypes = {
   products: PropTypes.array
+
 };
 
 // 구매하기 카드 리스트 형태
@@ -19,7 +20,7 @@ export default function ItemsList({ products, ...other }) {
     <Grid container spacing={6} {...other}>
       {products.map((product) => (
         <Grid sx={{ mb: 6 }} key={product.hash} item xs={12} sm={6} md={2.4}>
-          <ItemsCard product={product} />
+          <ItemsCard product={product}  />
         </Grid>
       ))}
     </Grid>
